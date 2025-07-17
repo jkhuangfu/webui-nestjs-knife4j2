16cd093c2b8cd3cce46004c298e7777e590b92c6
jkhuangfu <huangjiankang@sonicom.com.cn>
2025-07-17 17:33:36 +0800

    front project

---
+++ src/components/GlobalFooter/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="globalFooter">
+    <a-row v-if="settings.enableFooterCustom">
+      <Markdown v-if="settings.enableFooterCustom" :source="settings.footerCustomContent" />
+    </a-row>
+    <a-row v-else-if="settings.enableFooter">
+      <div class="copyright">Apache License 2.0 | Copyright
+        <a-icon type="copyright" />  Powered by <a target="_blank" href="https://github.com/jkhuangfu/nestjs-knife4j2">nestjs-knife4j2</a>
+      </div>
+    </a-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "GlobalFooter",
+  props: {
+    links: {
+      type: Array,
+      default: () => {
+        [];
+      }
+    }
+  },
+  components: {
+    "Markdown": () => import('@/components/Markdown')
+  },
+  computed: {
+    settings() {
+      return this.$store.state.globals.settings;
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "./index.less";
+</style>
+
